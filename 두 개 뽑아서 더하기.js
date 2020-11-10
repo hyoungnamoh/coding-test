92adc5fb00838e5be0ff442e3d259fe569e22b42
오형남 <zzzsh789@naver.com>
2020-11-10 23:02:23 +0900

    Add: 크레인 인형뽑기.js

--- 두 개 뽑아서 더하기.js
+++ 두 개 뽑아서 더하기.js
@@ -1,4 +1,18 @@
 
+const solution = (numbers) => {
+  let answer = [];
+  while (numbers.length !== 0) {
+    for (let i = 0; i < numbers.length; i++) {
+      if (answer.find(e => (numbers[numbers.length - 1] + numbers[i]) === e) !== 0 && !answer.find(e => (numbers[numbers.length - 1] + numbers[i]) === e)) {
+        if (numbers.length - 1 !== i) {
+          answer.push(numbers[numbers.length - 1] + numbers[i]);
+        }
+      };
+    }
+    numbers.pop();
+  }
+  return answer.sort((a, b) => a - b);
+}
 // 문제 설명
 // 정수 배열 numbers가 주어집니다. numbers에서 서로 다른 인덱스에 있는 두 개의 수를 뽑아 더해서 만들 수 있는 모든 수를 배열에 오름차순으로 담아 return 하도록 solution 함수를 완성해주세요.
 
